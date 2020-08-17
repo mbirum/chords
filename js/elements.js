b24258b39701545107efe6ad852256659a31f897
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2020-08-16 21:44:23 -0400

    size

--- js/elements.js
+++ js/elements.js
@@ -17,7 +17,7 @@ function setLayerSize(layer) {
     layer.height = 2000;
     layer.top = 0;
     layer.left = 0;
-    $(layer).width('90%');
+    $(layer).width('97%');
 }
 
 function setSliderSize(slider) {
